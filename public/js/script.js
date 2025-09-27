7626582614cb8b1b1e66efb353b90081678be26b
hitozz0 <wisnu.putra8506@gmail.com>
2025-09-27 23:00:09 +0800

    upd

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,9 @@
 $(function() {
+    $('.tambahData').on('click', function() {
+        $('#judulModal').html('Tambah data mahasiswa');
+    });
     $('.tampilModalUbah').on('click', function() {
         $('#judulModal').html('Ubah data mahasiswa');
     });
 });
+
