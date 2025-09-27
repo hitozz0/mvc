5b4bc08a88908579c3a577dc549d927627a49365
hitozz0 <wisnu.putra8506@gmail.com>
2025-09-27 22:34:07 +0800

    flasher upd

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -16,22 +16,26 @@ class Mahasiswa extends Controller{
     }
     public function tambah() {
         if ($this->model('Mahasiswa_model')->tambahDataMahasiswa($_POST) > 0) {
-            // Flasher::setFlash('berhasil', 'ditambahkan', 'success');
+            Flasher::setFlash('berhasil', 'ditambahkan', 'success');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         } else{
-            // Flasher::setFlash('gagal', 'ditambahkan', 'danger');
+            Flasher::setFlash('gagal', 'ditambahkan', 'danger');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
     }
+    public function edit(){
+        
+    }
+
     public function hapus($id) {
         if ($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0) {
-            // Flasher::setFlash('berhasil', 'dihapus', 'success');
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         } else{
-            // Flasher::setFlash('gagal', 'dihapus', 'danger');
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
             header('Location: ' . BASEURL . '/mahasiswa');
             exit;
         }
